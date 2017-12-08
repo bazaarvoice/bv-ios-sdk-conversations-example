7e56357d2cd3428b9f35b435edc846c4628f9c2f
Michael Van Milligan <michael.vanmilligan@bazaarvoice.com>
2017-12-08 15:43:50 -0600

    Clean up project settings.

--- Pods/Target Support Files/Pods-bv-ios-sdk-conversations-example/Pods-bv-ios-sdk-conversations-example-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_bv_ios_sdk_conversations_exampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_bv_ios_sdk_conversations_exampleVersionString[];
-
